ebdb1b5f45300ba26e6a205d2909160b9987bca9
Stephen Michel <smichel+git@jwplayer.com>
2023-08-22 16:43:42 -0400

    Render simple example query builder
    Or ranter, attempt to. Crashes the page.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { render } from 'preact';
 import preactLogo from './assets/preact.svg';
 import './style.css';
 
+import DemoQueryBuilder from './query-builder';
+
 export function App() {
 	return (
 		<div>
@@ -10,21 +12,7 @@ export function App() {
 			</a>
 			<h1>Get Started building Vite-powered Preact Apps </h1>
 			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
+				<DemoQueryBuilder />
 			</section>
 		</div>
 	);
